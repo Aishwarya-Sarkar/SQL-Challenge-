dc75e02ddc73557cf94029b88920c3c2e8600393
Aishwarya-Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-10 22:53:59 +0530

    Update Week1-DinerCaseStudy.sql

--- Week1-DinerCaseStudy.sql
+++ Week1-DinerCaseStudy.sql
@@ -48,3 +48,19 @@ DENse_RANK() OVER (partition by customer_id order by cnt desc) RN
 from A
 )c
 where c.rn=1;
+
+-- 6. Which item was purchased first by the customer after they became a member?
+WITH A AS
+(
+  SELECT
+      s.customer_id,
+      order_date,
+      dense_rank() over (partition by s.customer_id order by s.order_date asc) RN,
+      me.product_name
+  FROM dannys_diner.sales s
+  inner join dannys_diner.members m
+  on s.customer_id=m.customer_id and s.order_date>=m.join_date
+  inner join dannys_diner.menu me on s.product_id=me.product_id
+)
+SELECT customer_id, product_name from A
+where A.rn=1
